91167627541b3ef61abff6d4720897b64a511a54
Hemanth S <115523522+Hemanth672@users.noreply.github.com>
2024-01-31 17:17:28 +0530

    Update TaskListPage.jsx

--- src/comp/TaskListPage.jsx
+++ src/comp/TaskListPage.jsx
@@ -5,8 +5,7 @@ import NavigationBar from './Navbar';
 
 const TaskListPage = () => {
   return (
-    <div style={{backgroundImage:'url("https://img.freepik.com/free-photo/businessman-holding-hour-glass-signifies-importance-being-time_53876-13940.jpg?w=996&t=st=1705094802~exp=1705095402~hmac=20f2b4153a275c2ed99b10df9f2abbe6c1ad12f84ee2c08648a81aa9dd1de6bc")',height:347,
-    backgroundPosition: 'center'}}>
+    <div>
       <NavigationBar></NavigationBar>
     
       <Timer />
